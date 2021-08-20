0b3431b9261a7562b188d8377d3484044ebb0b9a
Thanh Vu <thanh@luongthien.com>
2021-08-20 13:27:17 +0700

    Initial Commit

---
+++ SourceLint/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SourceLint
+//
+//  Created by Thanh Vu on 20/08/2021.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SourceLint/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SourceLint
+//
+//  Created by Thanh Vu on 20/08/2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
